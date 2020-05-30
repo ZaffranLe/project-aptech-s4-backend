a944b5e2636b699fb12520e651995cdbc32d7a50
vukhoi1999 <vukhoi20021999@gmail.com>
2020-05-30 18:04:49 +0700

    add base

---
+++ ElectricShop/ElectricShop/Logger.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+
+namespace ElectricShop
+{
+    public class Logger
+    {
+        private static ILogger _logger;
+
+        public static void Write(string msg, bool isError = false)
+        {
+            if (_logger == null)
+                _logger = LogManager.GetCurrentClassLogger();
+            if (isError)
+                _logger.Error(msg);
+            else
+                _logger.Info(msg);
+        }
+    }
+}
