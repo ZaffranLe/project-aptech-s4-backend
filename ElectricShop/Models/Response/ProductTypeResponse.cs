52f68170b6a4a89c24139be2b6533011653857bf
khoi.vu <khoi.vu@quant-edge.com>
2020-06-16 17:48:46 +0700

    add res

---
+++ ElectricShop/Models/Response/ProductTypeResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ElectricShop.Entity.Entities;
+
+namespace ElectricShop.Models
+{
+    public class ProductTypeResponse : IDisposable
+    {
+        public ProductType ProductType { get; set; }
+        public  List<string> ListImagesUrl { get; set; }
+
+        public void Dispose()
+        {
+        }
+        public static string EntityName()
+        {
+            return typeof(ProductTypeResponse).Name;
+        }
+
+        public static string GetName()
+        {
+            return EntityName();
+        }
+
+    }
+}
---
+++ ElectricShop/Models/Response/ProviderResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ElectricShop.Entity.Entities;
+
+namespace ElectricShop.Models
+{
+    public class ProviderResponse : IDisposable
+    {
+        public Provider Provider{ get; set; }
+        public  List<string> ListImagesUrl { get; set; }
+
+        public void Dispose()
+        {
+        }
+        public static string EntityName()
+        {
+            return typeof(ProviderResponse).Name;
+        }
+
+        public static string GetName()
+        {
+            return EntityName();
+        }
+
+    }
+}
