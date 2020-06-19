1e306e7493437e6693762b132b3c93060c53cf5e
khoi.vu <khoi.vu@quant-edge.com>
2020-06-19 15:38:36 +0700

    update code

--- ElectricShop/Controllers/ProductController.cs
+++ ElectricShop/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using ElectricShop.Common.Enum;
@@ -13,272 +14,298 @@ using ElectricShop.Models;
 using ElectricShop.Utils;
 namespace ElectricShop.Controllers
 {
-	public class ProductController: ApiController
-	{
-		[EnableCors(origins: "*", headers: "*", methods: "*")]
-		public async Task<IHttpActionResult> Get()
-		{
-			try
-			{
-				#region token
-				var header = Request.Headers;
-				var token = header.Authorization.Parameter;
-				UserInfo userInfo;
-				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
-				{
-					return StatusCode(HttpStatusCode.Unauthorized);
-				}
-				#endregion
-				var lstData = MemoryInfo.GetAllProduct();
-			    List<ProductResponse> lstRes = new List<ProductResponse>();
+    public class ProductController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                #region token
+                var header = Request.Headers;
+                var token = header.Authorization.Parameter;
+                UserInfo userInfo;
+                if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
+                {
+                    return StatusCode(HttpStatusCode.Unauthorized);
+                }
+                #endregion
+                var lstData = MemoryInfo.GetAllProduct();
+                List<ProductResponse> lstRes = new List<ProductResponse>();
                 foreach (var product in lstData)
-			    {
-			        lstRes.Add(new ProductResponse
-			        {
+                {
+                    lstRes.Add(new ProductResponse
+                    {
                         Product = product,
-			            ListImages = ImagesUtils.GetImagesUrl(product.ImageId)
+                        ListImages = ImagesUtils.GetImagesUrl(product.ImageId)
                     });
                 }
-				var res = new RequestErrorCode(true, null, null);
+                var res = new RequestErrorCode(true, null, null);
                 res.ListDataResult.AddRange(lstRes);
-				return Ok(res);
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-			}
-			return BadRequest("Unknow");
-		}
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            return BadRequest("Unknow");
+        }
 
-		[EnableCors(origins: "*", headers: "*", methods: "*")]
-		public async Task<IHttpActionResult> Get(int id)
-		{
-			try
-			{
-				#region token
-				var header = Request.Headers;
-				var token = header.Authorization.Parameter;
-				UserInfo userInfo;
-				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
-				{
-					return StatusCode(HttpStatusCode.Unauthorized);
-				}
-				#endregion
-				var data = MemoryInfo.GetProduct(id);
-				var res = new RequestErrorCode(true, null, null);
-				res.ListDataResult.Add(new ProductResponse
-				{
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            try
+            {
+                #region token
+                var header = Request.Headers;
+                var token = header.Authorization.Parameter;
+                UserInfo userInfo;
+                if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
+                {
+                    return StatusCode(HttpStatusCode.Unauthorized);
+                }
+                #endregion
+                var data = MemoryInfo.GetProduct(id);
+                var res = new RequestErrorCode(true, null, null);
+                res.ListDataResult.Add(new ProductResponse
+                {
                     Product = data,
                     ListImages = ImagesUtils.GetImagesUrl(data.ImageId)
-				});
-				return Ok(res);
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-			}
-			return BadRequest("Unknow");
-		}
+                });
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            return BadRequest("Unknow");
+        }
 
-		[EnableCors(origins: "*", headers: "*", methods: "*")]
-		public async Task<IHttpActionResult> Post([FromBody]Product req)
-		{
-			try
-			{
-				string errorMessage = "UnknowError";
-				string errorCode = ErrorCodeEnum.UnknownError.ToString();
-				#region token
-				var header = Request.Headers;
-				var token = header.Authorization.Parameter;
-				UserInfo userInfo;
-				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
-				{
-					return StatusCode(HttpStatusCode.Unauthorized);
-				}
-				#endregion
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Post([FromBody]NewProductReq req)
+        {
+            try
+            {
+                string errorMessage = "UnknowError";
+                string errorCode = ErrorCodeEnum.UnknownError.ToString();
+                #region token
+                var header = Request.Headers;
+                var token = header.Authorization.Parameter;
+                UserInfo userInfo;
+                if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
+                {
+                    return StatusCode(HttpStatusCode.Unauthorized);
+                }
+                if (!Operator.HasPermision(userInfo.IdUserLogin, RoleDefinitionEnum.CreateProduct))
+                {
+                    return Ok(new RequestErrorCode(false, ErrorCodeEnum.Error_NotHavePermision.ToString(), "Khong co quyen tao moi"));
+                }
+                #endregion
+                #region Validate
+                if (!Validate(req, out errorCode, out errorMessage))
+                {
+                    return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+                }
+                #endregion
 
-				#region Validate
-				if (!Validate(req, out errorCode, out errorMessage))
-				{
-					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
-				}
-				#endregion
+                #region Tạo key
+                var productOldKey = Memory.Memory.GetMaxKey(Product.EntityName());
+                var propertyOldKey = Memory.Memory.GetMaxKey(Product.EntityName());
+                int productNewKey =  productOldKey + 1;
+                int propertyNewKey = propertyOldKey + 1;
+                var productInsert = req.Product;
+                productInsert.Id = productNewKey;// set key
 
-				#region Tạo key
-				var oldKey = Memory.Memory.GetMaxKey(req.GetName());
-				int newKey = oldKey + 1;
-				// set key
-				req.Id = newKey;
-				#endregion
+                #endregion
 
-				#region Process
-				req.CreatedAt = DateTime.Now;
-				req.CreatedBy = userInfo.IdUserLogin;
-				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
-				var lstCommand = new List<EntityCommand>();
-				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(req), EntityAction = EntityAction.Insert });
-				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
-				if (!isOkDone)
-				{
-					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
-				}
-				#endregion
-				// update memory
-				MemorySet.UpdateAndInsertEntity(req);
-				var result = new RequestErrorCode(true);
-				return Ok(result);
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-			}
-			return BadRequest("Unknow");
-		}
+                #region Process
+                productInsert.CreatedAt = DateTime.Now;
+                productInsert.CreatedBy = userInfo.IdUserLogin;
+                productInsert.UpdatedAt = DateTime.Now;
+                productInsert.UpdatedBy = userInfo.IdUserLogin;
+                UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+                var lstCommand = new List<EntityCommand>();
+                var lstUpdateMemory = new List<BaseEntity>();
+                foreach (var property in req.Properties)
+                {
+                    property.Id = propertyNewKey;
+                    property.IdProduct = productNewKey;
+                    property.CreatedAt = DateTime.Now;
+                    property.CreatedBy = userInfo.IdUserLogin;
+                    property.UpdatedAt = DateTime.Now;
+                    property.UpdatedBy = userInfo.IdUserLogin;
+                    lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(property), EntityAction = EntityAction.Insert });
+                    lstUpdateMemory.Add(property);
+                    propertyNewKey++;
+                }
+                
+                lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(productInsert), EntityAction = EntityAction.Insert });
+                lstUpdateMemory.Add(productInsert);
+                bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+                if (!isOkDone)
+                {
+                    return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+                }
+                #endregion
+                // update memory
+                foreach (var obj in lstUpdateMemory)
+                {
+                    MemorySet.UpdateAndInsertEntity(obj);
+                }
+                var result = new RequestErrorCode(true);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            return BadRequest("Unknow");
+        }
 
-		[EnableCors(origins: "*", headers: "*", methods: "*")]
-		public async Task<IHttpActionResult> Put(int id,[FromBody]Product req)
-		{
-			try
-			{
-				string errorMessage = "UnknowError";
-				string errorCode = ErrorCodeEnum.UnknownError.ToString();
-				#region token
-				var header = Request.Headers;
-				var token = header.Authorization.Parameter;
-				UserInfo userInfo;
-				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
-				{
-					return StatusCode(HttpStatusCode.Unauthorized);
-				}
-				#endregion
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Put(int id, [FromBody]Product req)
+        {
+            try
+            {
+                string errorMessage = "UnknowError";
+                string errorCode = ErrorCodeEnum.UnknownError.ToString();
+                #region token
+                var header = Request.Headers;
+                var token = header.Authorization.Parameter;
+                UserInfo userInfo;
+                if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
+                {
+                    return StatusCode(HttpStatusCode.Unauthorized);
+                }
+                #endregion
 
-				#region Validate
-				if (!ValidateUpdate(req, out errorCode, out errorMessage))
-				{
-					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
-				}
-				#endregion
+                #region Validate
+                if (!ValidateUpdate(req, out errorCode, out errorMessage))
+                {
+                    return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+                }
+                #endregion
 
-				#region Check exist
-				var obj = MemoryInfo.GetProduct(id);
-				if (obj == null)
-				{
-					return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
-				}
-				#endregion
-				req.Id = obj.Id; // gan lai id de update
-				#region Process
-				req.UpdatedAt = DateTime.Now;
-				req.UpdatedBy = userInfo.IdUserLogin;
-				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
-				var lstCommand = new List<EntityCommand>();
-				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(req), EntityAction = EntityAction.Update });
-				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
-				if (!isOkDone)
-				{
-					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
-				}
-				#endregion
-				// update memory
-				MemorySet.UpdateAndInsertEntity(req);
-				var result = new RequestErrorCode(true);
-				return Ok(result);
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-			}
-			return BadRequest("Unknow");
-		}
+                #region Check exist
+                var obj = MemoryInfo.GetProduct(id);
+                if (obj == null)
+                {
+                    return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
+                }
+                #endregion
+                req.Id = obj.Id; // gan lai id de update
+                #region Process
+                req.UpdatedAt = DateTime.Now;
+                req.UpdatedBy = userInfo.IdUserLogin;
+                UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+                var lstCommand = new List<EntityCommand>();
+                lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(req), EntityAction = EntityAction.Update });
+                bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+                if (!isOkDone)
+                {
+                    return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+                }
+                #endregion
+                // update memory
+                MemorySet.UpdateAndInsertEntity(req);
+                var result = new RequestErrorCode(true);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            return BadRequest("Unknow");
+        }
 
-		[EnableCors(origins: "*", headers: "*", methods: "*")]
-		public async Task<IHttpActionResult> Delete(int id)
-		{
-			try
-			{
-				string errorMessage = "UnknowError";
-				string errorCode = ErrorCodeEnum.UnknownError.ToString();
-				#region token
-				var header = Request.Headers;
-				var token = header.Authorization.Parameter;
-				UserInfo userInfo;
-				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
-				{
-					return StatusCode(HttpStatusCode.Unauthorized);
-				}
-				#endregion
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            try
+            {
+                string errorMessage = "UnknowError";
+                string errorCode = ErrorCodeEnum.UnknownError.ToString();
+                #region token
+                var header = Request.Headers;
+                var token = header.Authorization.Parameter;
+                UserInfo userInfo;
+                if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out userInfo))
+                {
+                    return StatusCode(HttpStatusCode.Unauthorized);
+                }
+                #endregion
 
-				#region Check exist
-				var obj = MemoryInfo.GetProduct(id);
-				if (obj == null)
-				{
-					return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
-				}
-				#endregion
+                #region Check exist
+                var obj = MemoryInfo.GetProduct(id);
+                if (obj == null)
+                {
+                    return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
+                }
+                #endregion
 
-				// check role 
-				if (!Operator.HasPermision(userInfo.IdUserLogin, RoleDefinitionEnum.None))
-				{
-				return Ok(new RequestErrorCode(false, ErrorCodeEnum.Error_NotHavePermision.ToString(), "Khong co quyen xoa"));
-				}
+                // check role 
+                if (!Operator.HasPermision(userInfo.IdUserLogin, RoleDefinitionEnum.None))
+                {
+                    return Ok(new RequestErrorCode(false, ErrorCodeEnum.Error_NotHavePermision.ToString(), "Khong co quyen xoa"));
+                }
 
-				#region Process
-				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
-				var lstCommand = new List<EntityCommand>();
-				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(obj), EntityAction = EntityAction.Delete });
-				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
-				if (!isOkDone)
-				{
-					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
-				}
-				#endregion
-				// update memory
-				MemorySet.RemoveEntity(obj);
-				var result = new RequestErrorCode(true);
-				return Ok(result);
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-			}
-			return BadRequest("Unknow");
-		}
+                #region Process
+                UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+                var lstCommand = new List<EntityCommand>();
+                lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(obj), EntityAction = EntityAction.Delete });
+                bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+                if (!isOkDone)
+                {
+                    return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+                }
+                #endregion
+                // update memory
+                MemorySet.RemoveEntity(obj);
+                var result = new RequestErrorCode(true);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            return BadRequest("Unknow");
+        }
 
-		#region Validation
-		private bool Validate(Product obj, out string errorCode, out string errorMess)
-		{
-			errorCode = null;
-			errorMess = null;
-			try
-			{
+        #region Validation
+        private bool Validate(NewProductReq obj, out string errorCode, out string errorMess)
+        {
+            errorCode = null;
+            errorMess = null;
+            try
+            {
 
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-				throw;
-			}
-			return true;
-		}
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+                throw;
+            }
+            return true;
+        }
 
-		private bool ValidateUpdate(Product obj, out string errorCode, out string errorMess)
-		{
-			errorCode = null;
-			errorMess = null;
-			try
-			{
+        private bool ValidateUpdate(Product obj, out string errorCode, out string errorMess)
+        {
+            errorCode = null;
+            errorMess = null;
+            try
+            {
 
-			}
-			catch (Exception ex)
-			{
-				Logger.Write(ex.ToString());
-				throw;
-			}
-			return true;
-		}
-		#endregion
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+                throw;
+            }
+            return true;
+        }
+        #endregion
 
 
-	}
+    }
 }
 
---
+++ ElectricShop/Models/Request/NewProductReq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ElectricShop.Entity.Entities;
+
+namespace ElectricShop.Models
+{
+    public class NewProductReq : IDisposable
+    {
+        public Product Product { get; set; }
+        public List<Property> Properties{ get; set; }
+
+        public void Dispose()
+        {
+        }
+        public static string EntityName()
+        {
+            return typeof(NewProductReq).Name;
+        }
+
+        public static string GetName()
+        {
+            return EntityName();
+        }
+
+    }
+}
